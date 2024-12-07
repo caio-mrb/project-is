c0e21049f5bb258087a87181d4bf8faf4751cce4
Caio Barbosa <caiomaxwel@hotmail.com>
2024-12-07 17:03:15 +0000

    Add controlers

---
+++ Project/Api/Controllers/ContainersController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Controllers
+{
+    public class ContainersController : Controller
+    {
+        // GET: Containers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Containers/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Containers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Containers/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Containers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Containers/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Containers/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Containers/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Project/Api/Controllers/RecordsController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Controllers
+{
+    public class RecordsController : Controller
+    {
+        // GET: Records
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Records/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Records/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Records/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Records/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Records/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Records/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Records/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
