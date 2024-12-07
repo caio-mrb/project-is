d87a2b301bda2459ff3990439880734fb599c400
Caio Barbosa <caiomaxwel@hotmail.com>
2024-12-07 17:07:00 +0000

    Remove unnecessary routes

--- Project/Api/Controllers/RecordsController.cs
+++ Project/Api/Controllers/RecordsController.cs
@@ -42,27 +42,6 @@ namespace Api.Controllers
             }
         }
 
-        // GET: Records/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Records/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
 
         // GET: Records/Delete/5
         public ActionResult Delete(int id)
