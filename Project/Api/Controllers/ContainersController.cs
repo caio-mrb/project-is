920b46e3d07340942eef35f98f1f64049da934d9
Caio Barbosa <caiomaxwel@hotmail.com>
2024-12-08 13:10:54 +0000

    Add get notifications and containers

--- Project/Api/Controllers/ContainersController.cs
+++ Project/Api/Controllers/ContainersController.cs
@@ -12,12 +12,63 @@ using System.Xml.Linq;
 
 namespace Api.Controllers
 {
-
+    [RoutePrefix("api/somiod/{appName}")]
     public class ContainersController : ApiController
     {
         string connectionString = Api.Properties.Settings.Default.ConnStr;
 
+        [HttpGet]
+        [Route("{contName}")]
+        public IHttpActionResult GetContainer(string appName, string contName)
+        {
+            Container c = null;
+            SqlConnection connection = null;
 
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.CommandText = @"
+                SELECT *
+                FROM dbo.containers c
+                JOIN dbo.applications a ON c.parent = a.id
+                WHERE c.name = @contName AND a.name = @appName";
+                command.Parameters.AddWithValue("@appName", appName);
+                command.Parameters.AddWithValue("@contName", contName);
+                command.CommandType = System.Data.CommandType.Text;
+                command.Connection = connection;
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    c = new Container();
+                    c.Id = (int)reader["id"];
+                    c.Name = (string)reader["name"];
+                    c.CreationDatetime = (DateTime)reader["creation_datetime"];
+                    c.Parent = (int)reader["parent"];
+                }
+                reader.Close();
+                return Ok(c);
+
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+            finally
+            {
+                try
+                {
+                    connection.Close();
+                }
+                catch
+                {
+
+                }
+            }
+        }
 
     }
 }
--- Project/Api/Controllers/NotificationsController.cs
+++ Project/Api/Controllers/NotificationsController.cs
@@ -12,11 +12,68 @@ using System.Xml.Linq;
 
 namespace Api.Controllers
 {
+    [RoutePrefix("api/somiod/{appName}/{contName}")]
     public class NotificationsController : ApiController
     {
         string connectionString = Api.Properties.Settings.Default.ConnStr;
 
+        [HttpGet]
+        [Route("{notiName}")]
+        public IHttpActionResult GetNotification(string appName, string contName, string notiName)
+        {
+            Notification n = null;
+            SqlConnection connection = null;
 
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.CommandText = @"
+                    SELECT *
+                    FROM dbo.notifications n
+                    JOIN dbo.containers c ON n.parent = c.id
+                    JOIN dbo.applications a ON c.parent = a.id
+                    WHERE a.name = @appName AND c.name = @contName AND n.name = @notiName";
+                command.Parameters.AddWithValue("@appName", appName);
+                command.Parameters.AddWithValue("@contName", contName);
+                command.Parameters.AddWithValue("@notiName", notiName);
+                command.CommandType = System.Data.CommandType.Text;
+                command.Connection = connection;
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    n = new Notification();
+                    n.Id = (int)reader["id"];
+                    n.Name = (string)reader["name"];
+                    n.CreationDatetime = (DateTime)reader["creation_datetime"];
+                    n.Parent = (int)reader["parent"];
+                    n.Event = (int)reader["event"];
+                    n.Endpoint = (string)reader["endpoint"];
+                    n.Enabled = (bool)reader["enabled"];
+                }
+                reader.Close();
+                return Ok(n);
+
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+            finally
+            {
+                try
+                {
+                    connection.Close();
+                }
+                catch
+                {
+
+                }
+            }
+        }
 
     }
 }
